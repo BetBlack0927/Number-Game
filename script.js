3c6ac4cdf53805412bb6d1be572ffaedd6d64680
BetBlack0927 <121522392+BetBlack0927@users.noreply.github.com>
2023-02-24 14:40:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,88 @@
 var i = 2;
 var user;
 var answer;
-var count = 5
+var count;
 var max =0
+var x= Math.floor(Math.random()*2)+1;
+var y=Math.floor(Math.random()*100)+1;
+var z=Math.floor(Math.random()*100)+1;
+var score = 0;
+var mode;
+
+function easy(){
+    mode = "Easy"
+    count = 15
+    start()
+    document.getElementById("easy").style.display="none"
+    document.getElementById("medium").style.display="none"
+    document.getElementById("hard").style.display="none"
+    factor = 5
+}
+
+function medium(){
+    mode = "Medium"
+    count =10
+    start()
+    document.getElementById("easy").style.display="none"
+    document.getElementById("medium").style.display="none"
+    document.getElementById("hard").style.display="none"
+    factor = 3  
+}
+
+function hard(){
+    mode = "Hard"
+    count = 5
+    start()
+    document.getElementById("easy").style.display="none"
+    document.getElementById("medium").style.display="none"
+    document.getElementById("hard").style.display="none"
+    factor = 2
+}
 
 function start(){
     document.getElementById("start").style.display="none"
+    if (x==1){
+        document.getElementById("Question").innerHTML=y+"+"+z
+        answer = y+z
+    }else{
+        document.getElementById("Question").innerHTML=y+"-"+z
+        answer = y-z
+    }
     document.getElementById("Title").style.display="block"
     document.getElementById("Question").style.display="block"
     document.getElementById("myText").style.display="block"
     document.getElementById("timer").style.display="block"
+    document.getElementById("mode").style.display="block"
+    document.getElementById("score").style.display="block"
+    document.getElementById("timer").innerHTML=count+" Seconds"
+    document.getElementById("mode").innerHTML=mode
 }
 
+
 function startTimer(){
-    myTimer = setInterval(countDown,500);
+    myTimer = setInterval(countDown,1000);
   }
 
 function countDown(){
     count -= 1;
     document.getElementById("timer").innerHTML=count+" Seconds"
-    console.log(count);
+    if(count<= 3){
+        document.getElementById("timer").style.backgroundColor="red"
+    }
+    if (count>3){
+        document.getElementById("timer").style.backgroundColor="#4CAF50"
+    }
     if (count <= max) {
         clearInterval(myTimer);
         console.log("finished");
-        document.getElementById("Question").innerHTML="Score: "+answer/2;
+        document.getElementById("Question").innerHTML+="="+answer;
         document.getElementById("myText").style.display="none"
         document.getElementById("mybutton").style.display="block"
 
     }
 }
 
-function yes(){
+function retry(){
     location.reload()
 }
 
@@ -40,25 +92,38 @@ document.addEventListener('DOMContentLoaded', () => {
     input.addEventListener('keyup', (e) => {
         console.log('keyup event triggered');
         if (e.keyCode === 13) {
-            Hello();
+            submit();
         }
     });
     
-    function Hello() {
+    function submit() {
         var user = document.getElementById("myText").value;
-    answer = i+i
+        var x= Math.floor(Math.random()*2)+1;
+        var y=Math.floor(Math.random()*100)+1;
+        var z=Math.floor(Math.random()*100)+1;
     if (user==""){
 
     }else
         if (user==answer){
-        i = i+i
-        document.getElementById("Question").innerHTML=i+"+"+i
-        count = count+2.5
+            if (x==1){
+                document.getElementById("Question").innerHTML=y+"+"+z
+
+            }else{
+                document.getElementById("Question").innerHTML=y+"-"+z
+            }
+        score = score+1
+        count = count+factor
+        document.getElementById("score").innerHTML="Score: "+score
+        if (x==1){
+            answer = y+z
+        }else{
+            answer = y-z
+        }
     }else{
-        document.getElementById("Question").innerHTML="Score: "+answer/2;
-        document.getElementById("myText").style.display="none"
-        document.getElementById("mybutton").style.display="block"
+        document.getElementById("myText").value = "";
     }
     document.getElementById("myText").value = "";
+
+    console.log(answer)
     }
 });
